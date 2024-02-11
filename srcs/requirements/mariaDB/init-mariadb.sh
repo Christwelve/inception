8a7d2b47182ed17b8adaff3dc552c85401b1d1c3
Christwelve <meng.christian28@gmail.com>
2024-02-11 10:02:00 +0100

    db login working

--- srcs/requirements/mariaDB/init-mariadb.sh
+++ srcs/requirements/mariaDB/init-mariadb.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-source .env
-# service mysql start;
+service mysql start;
 
-SQL_DATABASE=mariadb
-SQL_USER=cmeng
-SQL_PASSWORD=inception
+DB_NAME=mariadb
+DB_USER=cmeng
+DB_PASSWORD=inception
+# sudo systemctl start mariadb
 
-sudo systemctl start mariadb
+# sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+# sudo mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+# sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+# sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
-sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
-sudo mysql -u root -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
-sudo mysql -u root -e "FLUSH PRIVILEGES;"
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'localhost';"
-# mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
 
 # service mariadb stop;
 
